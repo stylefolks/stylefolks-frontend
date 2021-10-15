e60e15306360f4d3ed067ff041f634fa50d2e8a3
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-10-15 16:08:41 +0900

    [Add] 유저별 가입한 크루 받아오기

---
+++ src/__generated__/getUserCrew.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: getUserCrew
+// ====================================================
+
+export interface getUserCrew_getUserCrew_crews {
+  __typename: "Crew";
+  name: string;
+  id: number;
+  profileImg: string | null;
+}
+
+export interface getUserCrew_getUserCrew {
+  __typename: "GetUserCrewOutput";
+  ok: boolean;
+  error: string | null;
+  crews: getUserCrew_getUserCrew_crews[] | null;
+}
+
+export interface getUserCrew {
+  getUserCrew: getUserCrew_getUserCrew;
+}
+
+export interface getUserCrewVariables {
+  nickname: string;
+}
