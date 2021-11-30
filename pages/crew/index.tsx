13ff9be5f01a4e1910791e11199fca9326a52602
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-11-30 10:23:50 +0900

    [Add] Crew Main > Infinite Scroll

--- pages/crew/index.tsx
+++ pages/crew/index.tsx
@@ -2,9 +2,12 @@ import { useQuery } from '@apollo/client';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CircleProfileImage from 'components/common/CircleProfileImage';
+import PageChange from 'components/pageChange/PageChange';
 import gql from 'graphql-tag';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { getAllCrew, getAllCrewVariables } from 'src/__generated__/getAllCrew';
 import UtilStyle from 'styles/common/Util.module.scss';
 import CrewPageStyle from 'styles/crew/CrewPage.module.scss';
@@ -13,6 +16,8 @@ const GET_ALL_CREW = gql`
     getAllCrew(input: $input) {
       ok
       error
+      totalPages
+      totalResults
       crew {
         id
         profileImg
@@ -25,52 +30,94 @@ const GET_ALL_CREW = gql`
   }
 `;
 
-const Crew = () => {
-  const [inputTake, setInputTake] = useState<number | null>(null);
-  const [page, setPage] = useState<number>(1);
-  const { data, loading, error } = useQuery<getAllCrew, getAllCrewVariables>(
-    GET_ALL_CREW,
-    {
-      variables: {
-        input: {
-          page,
-          inputTake: 9,
-        },
-      },
-    }
-  );
+interface IProps {
+  params: any;
+}
 
-  console.log(data);
+const Crew = () => {
+  const [page, setPage] = useState<number>(1);
+  const router = useRouter();
+  const onError = () => {
+    router.push('/');
+  };
+
+  const { data, loading, error, refetch } = useQuery<
+    getAllCrew,
+    getAllCrewVariables
+  >(GET_ALL_CREW, {
+    variables: {
+      input: {
+        page,
+        inputTake: 9 * page,
+      },
+    },
+    onError,
+  });
+
+  if (loading) return <PageChange />;
+
+  console.log(page, data);
 
   return (
-    <div className={UtilStyle.mainContainer}>
-      <ul className={CrewPageStyle.crewMainPageContentsContainer}>
-        {data?.getAllCrew.crew.map((el) => (
-          <Link href={`/crew/${el.name}`} key={el.id}>
-            <a>
-              <li
-                className={CrewPageStyle.crewMainPageEachContents}
-                style={{
-                  backgroundImage: `url(${
-                    el.backgroundImg ? el.backgroundImg : ''
-                  })`,
-                }}
-              >
-                <div>
-                  <CircleProfileImage profileImg={el.profileImg} />
-                </div>
-                <h2>{el.name}</h2>
-                <h3>
-                  <FontAwesomeIcon icon={faUser} />
-                  {el.followerCount}
-                </h3>
-              </li>
-            </a>
-          </Link>
-        ))}
-      </ul>
-    </div>
+    <InfiniteScroll
+      dataLength={data?.getAllCrew.totalResults}
+      next={() => setPage((prev) => prev + 1)}
+      hasMore={page < data?.getAllCrew.totalPages}
+      loader={<h1>Loading..</h1>}
+      endMessage={<h4>No data</h4>}
+    >
+      <div className={UtilStyle.mainContainer}>
+        <ul className={CrewPageStyle.crewMainPageContentsContainer}>
+          {data?.getAllCrew.crew.map((el) => (
+            <Link href={`/crew/${el.name}`} key={el.id}>
+              <a>
+                <li
+                  className={CrewPageStyle.crewMainPageEachContents}
+                  style={{
+                    backgroundImage: `url(${
+                      el.backgroundImg ? el.backgroundImg : ''
+                    })`,
+                  }}
+                >
+                  <div>
+                    <CircleProfileImage profileImg={el.profileImg} />
+                  </div>
+                  <h2>
+                    {el.name}
+                    {'\u00A0'} <FontAwesomeIcon icon={faUser} />
+                    {el.followerCount}
+                  </h2>
+                  {/* <h3></h3> */}
+                </li>
+              </a>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </InfiniteScroll>
   );
 };
+
+// export const getStaticProps: GetStaticProps<IProps> = async ({ params }) => {
+//   const apolloClient = initializeApollo();
+
+//   const data = await apolloClient.query({
+//     query: GET_ALL_CREW,
+//     variable: {
+//       input: {
+//         page: 1,
+//         inputTake: 9,
+//       },
+//     },
+//   });
+
+//   console.log(data);
+
+//   return addApolloState(apolloClient, {
+//     props: {
+//       initialData: data,
+//     },
+//   });
+// };
 
 export default Crew;
