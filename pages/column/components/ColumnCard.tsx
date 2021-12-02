060624ba4a913aaa08151ecc5b4cdce9b5709e15
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-12-02 13:46:46 +0900

    [Add] Column Main > Layout

--- pages/column/components/ColumnCard.tsx
+++ pages/column/components/ColumnCard.tsx
@@ -1,8 +1,13 @@
-import { faFigma } from '@fortawesome/free-brands-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
 import Link from 'next/link';
+import VacantImage from 'public/vacantImage.png';
 import { getPostByCategory_getPostByCategory_post } from 'src/__generated__/getPostByCategory';
 import ColumnStyle from 'styles/column/ColumnPage.module.scss';
+import UtilStyle from 'styles/common/Util.module.scss';
+import { makePreContents } from 'utils/Utils';
+
 interface IColumnCard {
   data: getPostByCategory_getPostByCategory_post;
 }
@@ -10,17 +15,24 @@ interface IColumnCard {
 const ColumnCard: React.FC<IColumnCard> = ({ data }) => {
   return (
     <li className={ColumnStyle.cardWrapper}>
-      <Link href="">
+      <Link href={`post/${data.id}`}>
         <a>
-          <div>
-            <div>
-              {data.id}
-              <h2>TITLE {data.title}</h2>
-              <h4>Author{data.user.nickname}</h4>
+          <div className={UtilStyle.imageSquareContainer}>
+            <Image
+              alt={data.title}
+              src={data.titleImg || VacantImage}
+              layout="fill"
+            />
+          </div>
+
+          <div className={ColumnStyle.cardTextWrapper}>
+            <div className={ColumnStyle.cardTitleWrapper}>
+              <h2>{data.title}</h2>
+              <h4>{data.user.nickname}</h4>
             </div>
-            <span>Contents</span>
+            <span>{makePreContents(data.contents).slice(0, 100)}</span>
             <div>
-              <FontAwesomeIcon icon={faFigma} /> number: {data.viewCount}
+              <FontAwesomeIcon icon={faUsers} /> {data.viewCount}
             </div>
           </div>
         </a>
