72f4e7cc0b3149405bcf969f4821baad1203fd84
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-12-31 14:25:07 +0900

    [Modify] 텍스트 Eng > 한글

--- components/user/EditProfileImageModal.tsx
+++ components/user/EditProfileImageModal.tsx
@@ -2,7 +2,7 @@ import BackDrop from 'components/common/shared/BackDrop';
 import Modal from 'HOC/Modal';
 import React from 'react';
 import UploadModalStyle from 'styles/upload/UploadModal.module.scss';
-import useEditProfileImageModal from './hooks/useEditProfileImageModal';
+import useEditProfileImageModal from '../../hooks/pages/user/components/useEditProfileImageModal';
 
 interface IEditProps {
   doRefetch: () => void;
@@ -18,12 +18,12 @@ const EditProfileImageModal = ({ doRefetch }) => {
       <Modal visible={modal}>
         <BackDrop>
           <section className={UploadModalStyle.uploadModalContainer}>
-            <h2>Change Your Profile Image</h2>
+            <h2>프로필 이미지 변경</h2>
             <button name="upload" onClick={() => ref.current.click()}>
-              Upload Photo
+              새로운 이미지 등록하기
             </button>
-            <button onClick={onDelete}>Remove Current Photo</button>
-            <button onClick={onClick}>Cancel</button>
+            <button onClick={onDelete}>현재 프로필이미지 삭제하기</button>
+            <button onClick={onClick}>나가기</button>
             <input
               ref={ref}
               id="uploadProfileImage"
